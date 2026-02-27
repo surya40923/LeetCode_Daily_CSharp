1edc65fbf0044ab309882895485813a1aef234a9
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-02-27 22:09:22 +0530

    Add files via upload

---
+++ 0005_LeetProblem_67/0005_LeetProblem_67/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _0005_LeetProblem_67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a : ");
+            string a = Console.ReadLine();
+            Console.Write("Enter b : ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Result : "+AddBinary(a,b));
+        }
+
+        static string AddBinary(string a,string b)
+        {
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            int c = 0;
+            string res = "";
+            
+            while(i >= 0 || j >= 0 || c > 0)
+            {
+                int sum = c;
+                if (i >= 0) sum += a[i--] - '0';
+                if (j >= 0) sum += b[j--] - '0';
+
+                res = (sum % 2) + res;  
+                c = sum / 2;
+            }
+
+            return res;
+        }
+    }
+}
