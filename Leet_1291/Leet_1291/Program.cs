4c2755047232fa7e45a87ed3d89a12480f50416e
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-14 20:24:34 +0530

    Add files via upload

---
+++ Leet_1291/Leet_1291/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace Leet_1291
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Low : ");int low = int.Parse(Console.ReadLine());
+            Console.Write("Enter High : ");int high = int.Parse(Console.ReadLine());
+            IList<int> result = SequentialDigits(low, high);
+            Console.WriteLine("Output:");
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+
+        public static IList<int> SequentialDigits(int low,int high)
+        {
+            string sample = "123456789";
+            IList<int> nums = new List<int>();
+
+            for (int length = 2; length <= 9; length++)
+            {
+                for (int i = 0; i <= 9 - length; i++)
+                {
+                    string substring = sample.Substring(i, length);
+                    int num = int.Parse(substring);
+
+                    if (num >= low && num <= high)
+                    {
+                        nums.Add(num);
+                    }
+                }
+            }
+
+            return nums;
+        }
+    }
+}
