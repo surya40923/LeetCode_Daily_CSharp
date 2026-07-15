62ffa87671f0bdf07e4e4bfcc81f05297a613786
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-15 21:07:17 +0530

    Add files via upload

---
+++ Leet_3658/Leet_3658/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Leet_3658
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number : ");
+            int num = int.Parse(Console.ReadLine());
+            int output = GcdOfOddEvenSums(num);
+            Console.WriteLine("Output : "+output);
+
+        }
+
+        public static int GcdOfOddEvenSums(int n)
+        {
+            int sumOdd = 0;
+            int sumEven = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sumOdd += (2 * i - 1);
+                sumEven += (2 * i);
+            }
+            int result = GCD(sumOdd, sumEven);
+            return result;
+        }
+
+        private static int GCD(int a,int b)
+        {
+            while(b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
