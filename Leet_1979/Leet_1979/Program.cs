59bdd70d1e22dbc69c7e4ea8c69caf9dca07da16
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-18 21:07:03 +0530

    Add files via upload

---
+++ Leet_1979/Leet_1979/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Leet_1979
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array : ");
+            string input = Console.ReadLine();
+            int[] nums = input.Trim('[',']').Split(',').Select(int.Parse).ToArray();
+            Console.Write("Output : "+FindGcd(nums));
+        }
+
+        public static int FindGcd(int[] nums)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            foreach (int num in nums)
+            {
+                min = Math.Min(min, num);
+                max = Math.Max(max, num);
+            }
+            return Gcd(min, max);
+        }
+
+        public static int Gcd(int a,int b)
+        {
+            while(b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
