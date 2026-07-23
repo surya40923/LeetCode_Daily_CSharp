a906e7b1173dc5d7ab3b77b306e4078159538bb6
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-23 21:50:09 +0530

    Add files via upload

---
+++ Leet_3513/Leet_3513/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Leet_3513
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Nums : ");string input = Console.ReadLine();
+            int[] nums = input.Trim('[',']').Split(",").Select(int.Parse).ToArray();
+            Console.Write("Output : "+UniqueXorTriplets(nums));
+        }
+
+        public static int UniqueXorTriplets(int[] nums)
+        {
+            int n = nums.Length;
+            if (n <= 2) return n;
+            int ans = 1;
+            while (ans <= n) ans <<= 1;
+            return ans;
+        }
+    }
+}
