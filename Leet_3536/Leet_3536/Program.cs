d66c8ffa560e969275efa740029c9ec3eb005c23
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-25 21:49:13 +0530

    Add files via upload

---
+++ Leet_3536/Leet_3536/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Leet_3536
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num : ");
+            int input = int.Parse(Console.ReadLine());
+            Console.Write("Output : " + MaxProduct(input));
+        }
+
+        public static int MaxProduct(int n)
+        {
+            int first = 0; int second = 0;
+            while(n > 0)
+            {
+                int x = n % 10;
+                if( x > first )
+                {
+                    second = first;
+                    first = x;
+                }
+                else if (x > second)
+                {
+                    second = x;
+                }
+                n /= 10;
+            }
+            return first * second;
+        }
+    }
+}
