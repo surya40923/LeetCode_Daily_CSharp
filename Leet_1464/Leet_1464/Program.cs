e608be7c0a3e49b952950a8719bac13758fc8773
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-28 21:49:27 +0530

    Add files via upload

---
+++ Leet_1464/Leet_1464/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Leet_1464
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array : ");
+            string input = Console.ReadLine();
+            int[] nums = input.Trim('[',']').Split(",").Select(int.Parse).ToArray();
+            Console.Write("Output : " + MaxProduct(nums));
+        }
+
+        public static int MaxProduct(int[] nums)
+        {
+            /*
+            int[] r = nums.OrderByDescending(x => x).Take(2).ToArray();
+            return (r[0] - 1) * (r[1] - 1);
+            */
+
+            int max1 = 0, max2 = 0;
+
+            foreach (int num in nums)
+            {
+                if (num > max1)
+                {
+                    max2 = max1;
+                    max1 = num;
+                }
+                else if (num > max2)
+                {
+                    max2 = num;
+                }
+            }
+
+            return (max1 - 1) * (max2 - 1);
+        }
+    }
+}
