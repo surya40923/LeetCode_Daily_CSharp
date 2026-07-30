af45614afe8ab93c5f0d2ebdabdbf9a2c85d9884
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-30 22:09:56 +0530

    Add files via upload

---
+++ Leet_3014/Leet_3014/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Leet_3014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter word : ");string word = Console.ReadLine();
+            Console.Write("Output : "+MinimumPushes(word));
+        }
+
+        public static int MinimumPushes(string word)
+        {
+            int total = 0;
+            for(int i = 0;i < word.Length;i++)
+            {
+                total += (i / 8) + 1;
+            }
+            return total;
+        }
+    }
+}
