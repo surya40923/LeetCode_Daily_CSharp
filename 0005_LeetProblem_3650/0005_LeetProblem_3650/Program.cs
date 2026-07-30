12104baabf2f035ed800f05ead66260557c5ea62
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-30 22:10:50 +0530

    Add files via upload

---
+++ 0005_LeetProblem_3650/0005_LeetProblem_3650/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _0005_LeetProblem_3650
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter Array : ");
+            string input = Console.ReadLine();
+            int[][] arr = input.Trim('[', ']').Split("],[").Select(row => row.Trim('[',']').Split(",").Select(int.Parse).ToArray()).ToArray();
+            foreach (var row in arr)
+            {
+                Console.WriteLine($"[{string.Join(",", row)}]");
+            }
+        }
+    }
+}
