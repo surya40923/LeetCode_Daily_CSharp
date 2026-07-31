d57a069a8b6742c56501cb0cf2ca7086f68e0f30
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-07-31 13:20:19 +0530

    Add files via upload

---
+++ Leet_3016/Leet_3016/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Leet_3016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string : ");string s = Console.ReadLine();
+            Console.Write("Output : "+MinimumPushes(s));
+        }
+
+        public static int MinimumPushes(string word)
+        {
+            int[] freq = new int[26];
+            foreach(char c in word)
+            {
+                freq[c - 'a']++;
+            }
+            Array.Sort(freq);
+            int push = 0;
+            for(int i = 25;i >= 0;i--)
+            {
+                if (freq[i] == 0)
+                {
+                    break;
+                }
+                push += ((25 - i) / 8 + 1) * freq[i];
+            }
+            return push;
+        }
+    }
+}
