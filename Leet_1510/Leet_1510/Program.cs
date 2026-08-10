1ed5bf93d8ed90b2a58b20efee2f515f94bdcf53
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-10 20:58:11 +0530

    Add files via upload

---
+++ Leet_1510/Leet_1510/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Leet_1510
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n : ");int n = int.Parse(Console.ReadLine());
+            Console.Write("Output : "+WinnerSquareGame(n));
+        }
+
+        public static bool WinnerSquareGame(int n)
+        {
+            bool[] dp = new bool[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                for (int k = 1; k * k <= i; k++)
+                {
+                    if (!dp[i - k * k])
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[n];
+        }
+    }
+}
