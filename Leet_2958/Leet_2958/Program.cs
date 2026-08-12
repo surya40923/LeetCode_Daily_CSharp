17f88c0cc380711cd6b4e6593c239c3e5fa45f8d
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-12 21:50:30 +0530

    Add files via upload

---
+++ Leet_2958/Leet_2958/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Leet_2958
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array : ");string input = Console.ReadLine();
+            int[] nums = input.Trim('[', ']').Split(',').Select(int.Parse).ToArray();
+            Console.Write("Enter k : ");int k = int.Parse(Console.ReadLine());
+            Console.Write("Output : "+MaxSubarrayLength(nums,k));
+        }
+
+        public static int MaxSubarrayLength(int[] nums, int k)
+        {
+            int ans = 0;int start = -1;
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+            for(int end = 0;end < nums.Length; end++)
+            {
+                if (!frequency.ContainsKey(nums[end]))
+                    frequency[nums[end]] = 0;
+
+                frequency[nums[end]]++;
+
+                while (frequency[nums[end]] > k)
+                {
+                    start++;
+                    frequency[nums[start]]--;
+                }
+
+                ans = Math.Max(ans, end - start);
+            }
+            return ans;
+        }
+    }
+}
