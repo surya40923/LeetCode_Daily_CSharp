5b9f074e66f73cbf8c5101345c3fd950e7c3ea75
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-16 19:46:24 +0530

    Add files via upload

---
+++ Leet_3702/Leet_3702/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Leet_3702
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array : ");string arr = Console.ReadLine();
+            int[] nums = arr.Trim('[',']').Split(',').Select(int.Parse).ToArray();
+            Console.Write("Output : "+LongestSubsequence(nums));
+        }
+
+        public static int LongestSubsequence(int[] nums)
+        {
+            int n = nums.Length;
+            int totalXor = 0;
+            bool allZero = true;
+            foreach(int x in nums)
+            {
+                totalXor ^= x;
+                if(x > 0) allZero = false;
+            }
+            if (totalXor > 0) return n;
+            return allZero ? 0 : n - 1;
+        }
+    }
+}
