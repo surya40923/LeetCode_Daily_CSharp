fb3fa88c80c8cdb46e4b89c39d2f9992a0a82aad
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-18 21:37:30 +0530

    Add files via upload

---
+++ Leet_3471/Leet_3471/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Leet_3471
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array : ");string input = Console.ReadLine();
+            int[] nums = input.Trim('[',']').Split(',').Select(int.Parse).ToArray();
+            Console.Write("Enter k : ");int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Output : "+LargestInteger(nums,k));
+        }
+
+        public static int LargestInteger(int[] nums, int k)
+        {
+            if (k == nums.Length)
+            {
+                return nums.Max();
+            }
+
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (freq.ContainsKey(num))
+                    freq[num]++;
+                else
+                    freq[num] = 1;
+            }
+
+            int res = -1;
+
+            if (k == 1)
+            {
+                foreach (int num in nums)
+                {
+                    if (freq[num] == 1)
+                    {
+                        res = Math.Max(res, num);
+                    }
+                }
+
+                return res;
+            }
+
+            if (freq[nums[0]] == 1)
+            {
+                res = nums[0];
+            }
+
+            if (freq[nums[nums.Length - 1]] == 1)
+            {
+                res = Math.Max(res, nums[nums.Length - 1]);
+            }
+
+            return res;
+        }
+    }
+}
