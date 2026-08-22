f28209183ef5870dd4fa8570bc77dfaefc467c9f
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-22 19:32:12 +0530

    Add files via upload

---
+++ Leet_3622/Leet_3622/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Leet_3622
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Input : ");int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Output : "+CheckDivisibility(n));
+        }
+
+        public static bool CheckDivisibility(int n)
+        {
+            int digit_sum = 0;
+            int digit_product = 1;
+            int temp = n;
+
+            while(temp > 0)
+            {
+                int digit = temp % 10;
+                temp = temp / 10;
+
+                digit_sum = digit_sum + digit;
+                digit_product = digit_product * digit;
+            }
+
+            return n % (digit_sum + digit_product) == 0;
+        }
+    }
+}
