a0ebb8908dd2e9e9c97d7702ac9c95b182aec13e
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-08-25 18:15:44 +0530

    Add files via upload

---
+++ Leet_3718/Leet_3718/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Leet_3718
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array : ");string arr = Console.ReadLine();
+            int[] nums = arr.Trim('[',']').Split(',').Select(int.Parse).ToArray();
+            Console.Write("Enter k : ");int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Output : "+MissingMultiple(nums,k));
+        }
+
+        public static int MissingMultiple(int[] nums, int k)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            foreach(int num in nums)
+            {
+                seen.Add(num);
+            }
+
+            int ans = k;
+            while(seen.Contains(ans))
+            {
+                ans += k;
+            }
+            return ans;
+        }
+    }
+}
