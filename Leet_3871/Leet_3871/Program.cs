b726544c6a6b09088a27542d09a917639570c115
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-09-09 12:58:45 +0530

    Add files via upload

---
+++ Leet_3871/Leet_3871/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Leet_3871
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num : ");
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine("Output : "+CountCommas(n));
+        }
+
+        public static long CountCommas(long n)
+        {
+            long p = 1000;
+            long res = 0;
+            while (p <= n)
+            {
+                res += n - p + 1;
+                p *= 1000;
+            }
+            return res;
+        }
+    }
+}
