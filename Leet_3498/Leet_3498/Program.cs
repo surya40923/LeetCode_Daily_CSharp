5f82b2352d19209f4e096bab7fa5793b65944335
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-09-20 19:28:40 +0530

    Add files via upload

---
+++ Leet_3498/Leet_3498/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Leet_3498
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String : ");string input = Console.ReadLine();
+            Console.WriteLine("Output : "+ReverseDegree(input));
+        }
+
+        public static int ReverseDegree(string s)
+        {
+            int ans = 0;
+            for(int i = 0;i < s.Length;i++)
+            {
+                ans += (26 - (s[i] - 'a')) * (i + 1);
+            }
+            return ans;
+        }
+    }
+}
