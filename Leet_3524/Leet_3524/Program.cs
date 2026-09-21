811810550588ffb2511a6fb72266c3ffcd42bf8b
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-09-21 18:43:42 +0530

    Add files via upload

---
+++ Leet_3524/Leet_3524/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Leet_3524
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array : ");string arr = Console.ReadLine();
+            int[] nums = arr.Trim('[', ']').Split(",").Select(int.Parse).ToArray();
+            Console.Write("Enter k : ");int k = int.Parse(Console.ReadLine());
+            long[] res = ResultArray(nums, k);
+            Console.WriteLine("Ouput : ["+String.Join(",",res)+"]");
+        }
+
+        public static long[] ResultArray(int[] nums, int k)
+        {
+            long[] result = new long[k];
+            long[] dp = new long[k];
+            foreach(int num in nums)
+            {
+                long[] nextdp = new long[k];
+                nextdp[num % k] += 1;
+                for(int r = 0;r < k;r++)
+                {
+                    if (dp[r] > 0)
+                    {
+                        int newr = (int)(((long)r * num) % k);
+                        nextdp[newr] += dp[r];
+                    }
+                }
+                dp = nextdp;
+                for(int r = 0;r < k;r++)
+                {
+                    result[r] += dp[r];
+                }
+            }
+            return result;
+        }
+
+    }
+}
