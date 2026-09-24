1dc5919f9d4957341b6ed42e1a526bb16fb9f7b2
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-09-24 19:21:56 +0530

    Add files via upload

---
+++ Leet_3550/Leet_3550/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Leet_3550
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array : ");string input = Console.ReadLine();
+            int[] nums = input.Trim('[', ']').Split(",").Select(int.Parse).ToArray();
+            Console.WriteLine("Output : "+SmallestIndex(nums));
+        }
+
+        public static int SmallestIndex(int[] nums)
+        {
+            for(int i = 0;i < nums.Length;i++)
+            {
+                if(getDigit(nums[i]) == i)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int getDigit(int num)
+        {
+            int total = 0;
+            while(num > 0)
+            {
+                total += num % 10;
+                num /= 10;
+            }
+            return total;
+        }
+    }
+}
