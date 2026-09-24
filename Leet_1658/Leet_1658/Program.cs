9a9457d889456c01f689437e782774f22e668440
surya40923 <142232205+surya40923@users.noreply.github.com>
2026-09-24 19:09:46 +0530

    Add files via upload

---
+++ Leet_1658/Leet_1658/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Leet_1658
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array : ");string arr = Console.ReadLine();
+            int[] nums = arr.Trim('[',']').Split(",").Select(int.Parse).ToArray();
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Output : "+MinOperations(nums,x));
+        }
+
+        public static int MinOperations(int[] nums, int x)
+        {
+            int totalSum = 0;
+            foreach (int num in nums) totalSum += num;
+
+            int target = totalSum - x;
+            if (target < 0) return -1;
+
+            int maxLen = -1;
+            int currentSum = 0;
+            int left = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                currentSum += nums[right];
+
+                while (currentSum > target && left <= right)
+                {
+                    currentSum -= nums[left];
+                    left++;
+                }
+
+                if (currentSum == target)
+                {
+                    maxLen = Math.Max(maxLen, right - left + 1);
+                }
+            }
+
+            return maxLen == -1 ? -1 : nums.Length - maxLen;
+        }
+    }
+}
